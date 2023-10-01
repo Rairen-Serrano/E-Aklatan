ac2195ca5538bd8ce5a12ca60d87c05528d33e94
Rairen Serrano <rairen.serrano@yahoo.com>
2023-10-01 22:41:21 +0800

    navbar and footer update

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//Universal Script
+
+/*  ===================   SERRANO SCRIPT   ===================*/
+
+// to use navbar.html repeatedly by placing <div id="nav-placeholder"></div> in top of the body tag
+  $(function(){
+    $("#navbar-placeholder").load("navbar.html");
+    $("#footer-placeholder").load("footer.html");
+  });
+
+  
+/*  ===================   WAYCO SCRIPT   ===================*/
+
+/*  ===================   VICQUERRA SCRIPT   ===================*/
+
+/*  ===================   TOBIAS SCRIPT   ===================*/
