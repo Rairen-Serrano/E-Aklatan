541525421dcd229a41dd1b90a09cafb5c8b90d9a
TAMAYO KENN ANGELO <kenntamayo6@gmail.com>
2024-05-13 10:14:19 +0800

    dbconnect changes

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 
-    $dbconnect = mysqli_connect("localhost", "root", "", "e-aklatan");
+    $dbconnect = mysqli_connect("localhost", "u166465130_eaklatan", "OXvEC4L@6f", "u166465130_eaklatan");
 
     if(mysqli_connect_errno()) {
         echo "Connection failed:".mysqli_connect_error();
